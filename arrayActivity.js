f3e7fde6f9b6de3ce191442785efff2b258a18d0
HACHILES, MATHEW EDGIE H. <mathewedgieh@gmail.com>
2025-03-30 22:02:47 +0800

    initial commit

---
+++ arrayActivity.js
@@ -0,0 +1,12 @@
+//Activity 1: Working with Arrays
+let favSingers = ["Moira Dela Torre", "Tj Monterde", "Amiel Sol"];
+console.log("First favorite singer:", favSingers[0]);
+
+let favNumbers = [5, 14, 3, 29];
+console.log("Favorite numbers:", favNumbers);
+
+let mixedArr = ["Hello, world!", ["Nested array"], 123, true];
+console.log("Mixed array items:");
+for (let i = 0; i < mixedArr.length; i++) {
+  console.log(mixedArr[i]);
+}
